6c0eeb38182d64b1c881dc2ef4e6896c64dc1f0f
AndreaLassaga <63156387+AndreaLassaga@users.noreply.github.com>
2020-06-28 15:06:00 -0300

    Agrego sonido Gol

--- TGC.Group/Model/EscenaJuego.cs
+++ TGC.Group/Model/EscenaJuego.cs
@@ -15,6 +15,7 @@ using TGC.Core.Shaders;
 using TGC.Core.Terrain;
 using TGC.Core.Text;
 using TGC.Group.Model._2D;
+using TGC.Core.Sound;
 
 namespace TGC.Group.Model
 {
@@ -57,6 +58,7 @@ namespace TGC.Group.Model
         private Microsoft.DirectX.Direct3D.Effect effect;
         private CubeTexture g_pCubeMap; // Cubemap para Env Shader
         int frameNumber; // Numero de frame
+        private TgcMp3Player mp3Gol;
 
         public EscenaJuego(TgcCamera Camera, string MediaDir, string ShadersDir, TgcText2D DrawText, float TimeBetweenUpdates, TgcD3dInput Input, List<Jugador> jugadores, Jugador jugadorActivo, bool dia = true) : base(Camera, MediaDir, ShadersDir, DrawText, TimeBetweenUpdates, Input)
         {
@@ -73,7 +75,8 @@ namespace TGC.Group.Model
 
             sol = new Luz(Color.White, new TGCVector3(0, 70, -130));
 
-
+            mp3Gol = new TgcMp3Player();
+            mp3Gol.FileName = MediaDir + "Music\\Gol.mp3";
             pelota = new Pelota(escena.getMeshByName("Pelota"), new TGCVector3(0f, 50f, -250f));
             pelota.Mesh.Effect = TGCShaders.Instance.LoadEffect(ShadersDir + "CustomShaders.fx");
             pelota.Mesh.Technique = "BlinnPhong";
@@ -192,6 +195,7 @@ namespace TGC.Group.Model
         private void Reubicar()
         {
             pelota.ReiniciarPelota();
+            mp3Gol.closeFile();
             jugadores.ForEach(jugador => jugador.ReiniciarJugador());
             turbos.ForEach(turbo => turbo.Reiniciar());
         }
@@ -253,12 +257,14 @@ namespace TGC.Group.Model
                 if (arcos[0].CheckCollideWith(pelota))
                 {
                     golequipo1++;
+                    mp3Gol.play(true);
                     animacionGol.AnimarGol(jugadores, Color.Blue);
                 }
 
                 if (arcos[1].CheckCollideWith(pelota))
                 {
                     golequipo2++;
+                    mp3Gol.play(true);
                     animacionGol.AnimarGol(jugadores, Color.Red);
                 }
             }
